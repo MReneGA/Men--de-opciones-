e9283efa6f57eb9ed60083df1de54e1d93045846
MReneGA <rene_gamboa19@hotmail.com>
2012-10-29 10:52:41 -0600

    Agregar archivo del proyecto

---
+++ menu de opciones/src/menu/de/opciones/MenuDeOpciones.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu.de.opciones;
+
+/**
+ *
+ * @author manuel rene
+ */
+public class MenuDeOpciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
